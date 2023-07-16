11fbff25a17297c7c91a14b516404df531d0aacf
Batuhan Bayram <batu_byram@hotmail.com>
2023-07-16 16:21:32 +0300

    Hafta1-Medium

---
+++ Hafta-1/Soru-2/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace hackerrank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ?userinput = Console.ReadLine();
+
+            if (userinput != null)
+            {
+                Dictionary<char, int> kvp = new Dictionary<char, int>();
+
+                for (int i = 0; i < userinput.Length; i++)
+                {
+                    char c = userinput[i];
+                    if (kvp.ContainsKey(c))
+                    {
+                        kvp[c]++;
+                    }
+                    else
+                    {
+                        kvp.Add(c, 1);
+                    }
+                }
+                KeyValuePair<char, int> maxPair = new KeyValuePair<char, int>(char.MinValue,int.MinValue);
+
+
+                foreach (KeyValuePair<char, int> kvp2 in kvp)
+                {
+                    if(kvp2.Value >  maxPair.Value)
+                    {
+                        maxPair = kvp2;
+                    }
+                }
+                Console.WriteLine("Most repeated letter:");
+                Console.WriteLine("Letter: " + maxPair.Key);
+                Console.WriteLine("Value: " + maxPair.Value);
+
+
+            }
+
+                    }
+    }
+}
+
+
+
+
+
+
+
+
