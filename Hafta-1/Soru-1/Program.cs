31acd31b09c221669c5c826372eff576401db78e
Batuhan Bayram <batu_byram@hotmail.com>
2023-07-16 04:14:24 +0300

    Hafta1-Easy

---
+++ Hafta-1/Soru-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your birth day (exp: aa/bb/cccc): ");
+        string userInput = Console.ReadLine();
+
+        DateTime date;
+        DateTime today = DateTime.Today;
+
+        if (DateTime.TryParse(userInput, out date))
+        {
+            int age = today.Year - date.Year;
+            Console.WriteLine("Your age is: " + age);
+        }
+        else
+        {
+            Console.WriteLine("Not valid.");
+        }
+    }
+}
